4be0e04e7a2538adf4f6e72b74d83297ae14d124
eliabjselvacruz <eliabjselvacruz51@gmail.com>
2023-01-01 16:56:47 -0600

    Practicing text formatting.

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string cadena;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("{0} {1}", "¡Hola", "mundo!");
+string nombre = "Eliab";
+cadena = String.Format("Nombre = {0}.\nHora actual = {1:hh}:{1:mm}", nombre, DateTime.Now);
+
+Console.WriteLine(cadena);
+
+Console.ReadKey();
